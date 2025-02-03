02bdca80d7c9ceabc8ca4f29f2ff544745f11b5e
Mark-andrew7 <kamaumark19@gmail.com>
2025-02-03 06:09:34 +0000

    Initial project setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Number classifictaion API is running" });
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
+
